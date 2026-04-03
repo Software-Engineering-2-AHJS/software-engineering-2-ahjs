9454a9c33c12804d74e00bf27e76e5457194af26
parxevicj <suryakiranpradosh@gmail.com>
2026-04-03 17:08:56 +0300

    add api route for invoice retrieval. modify dashboard to fetch from api// do it aaaaaalllllllll again

--- app/dashboard/client/dashboard.tsx
+++ app/dashboard/client/dashboard.tsx
@@ -1,6 +1,6 @@
 
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 type InvoiceStatus = "Approved" | "Draft" | "Processing";
 type InvoiceType = "Income" | "Expense";
@@ -120,8 +120,8 @@ const mockInvoices: Invoice[] = [
   },
 ];
 
-function getInvoice(): Invoice[] {
-  return mockInvoices;
+async function getInvoice(): Promise<Invoice[]> {
+  return (await fetch("/api/invoice")).json();
 }
 
 function UploadInvoiceModal({
@@ -238,7 +238,10 @@ function UploadInvoiceModal({
   );
 }
 export default function Dashboard() {
-  const [invoices, setInvoices] = useState<Invoice[]>(getInvoice());
+  const [invoices, setInvoices] = useState<Invoice[]>([]);
+  useEffect(() => {
+    getInvoice().then((x) => setInvoices(x));
+  }, []);
 
   const [modalOpen, setModalOpen] = useState<boolean>(false);
   const [sidebarOpen, setSidebarOpen] = useState<boolean>(true);
